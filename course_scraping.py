357cce512db522a0d080b5ced44f20a6b63e82d1
austin <austinsilveria@gmail.com>
2019-07-10 22:00:08 -0700

    Updated CourseScraper to use FirebaseProxy

--- course_scraping.py
+++ course_scraping.py
@@ -3,64 +3,96 @@
 
 import requests
 from bs4 import BeautifulSoup
-import openpyxl
-from time import sleep
 
-def get_soup(link):
-    return BeautifulSoup((requests.get(link)).text, 'html.parser')
 
-# Retrieves department list from Cal Poly
-top_link = 'http://catalog.calpoly.edu/coursesaz/'
-top_soup = get_soup(top_link)
+from firebase.firebase_proxy import FirebaseProxy
+from transaction import Transaction
 
-departments_az = top_soup.find(id="/coursesaz/")
-department_urls = [department.get('href') for department in departments_az.find_all('a')]
 
-# Retrieves course info for each department
-for department in department_urls:
-    sleep(1)
-    # Extracts the department name from the URL
-    dep_name = (department.rsplit('/',2)[1]).upper()
+class CourseScraper:
+    # Firestore client parameter must be named "firebase_proxy" for dependency
+    # injection to work
+    # (https://github.com/google/pinject#basic-dependency-injection)
+    def __init__(self, firebase_proxy: FirebaseProxy):
+        self.firebase_proxy = firebase_proxy
 
-    # Creates a new spreadsheet
-    wb = openpyxl.Workbook()
-    main = wb.active
-    main.title = dep_name
-    main["A1"] = 'Course name'
-    main["B1"] = 'Units'
-    main["C1"] = 'Prerequisites'
-    main["D1"] = 'Terms Typically Offered'
-    row = 2
+        # Collection where all scraped documents will go
+        self.COLLECTION = "Scraped"
+        # Document type of a course entry
+        self.DOCUMENT_TYPE = "course"
 
-    # Gets raw list of courses and info for department
-    dep_link = 'http://catalog.calpoly.edu' + department
-    dep_soup = get_soup(dep_link)
-    courses = dep_soup.findAll("div", {"class": "courseblock"})
+    @staticmethod
+    def get_soup(link):
+        return BeautifulSoup((requests.get(link)).text, 'html.parser')
 
-    # Extract info for each class and populate spreadsheet
-    for course in courses:
-        course_name_and_units = (course.find("p", {"class": "courseblocktitle"})).get_text()
-        course_name, course_units = course_name_and_units.splitlines()
-        course_units = course_units.split(' ',1)[0]
+    def parse(self):
+        # Retrieves department list from Cal Poly
+        top_link = 'http://catalog.calpoly.edu/coursesaz/'
+        top_soup = self.get_soup(top_link)
 
-        course_terms_and_prereqs = (course.find("div", {"class": "noindent courseextendedwrap"})).get_text()
-        try:
-            course_terms, course_prereqs = course_terms_and_prereqs.split("Prerequisite: ")
-        except IndexError:
-            course_prereqs = "NA"
-            course_terms = course_terms_and_prereqs.split("Term Typically Offered: ",1)[1]
-        except ValueError:
-            # Yes this is the same behavior as an IndexError
-            course_prereqs = "NA"
-            course_terms = course_terms_and_prereqs.split("Term Typically Offered: ",1)[1]
-        else:
-            course_terms = course_terms.split("Term Typically Offered: ",1)[1]
+        departments_az = top_soup.find(id="/coursesaz/")
+        department_urls = [department.get('href')
+                           for department in departments_az.find_all('a')]
 
-        row_string = str(row)
-        main["A" + row_string] = course_name
-        main["B" + row_string] = course_units
-        main["C" + row_string] = course_prereqs
-        main["D" + row_string] = course_terms
-        row += 1
+        # This will store all courses that will be added to firebase. These
+        # will all be written at once with a "batch write" to avoid calling
+        # firebase a high number of times
+        firebase_transactions = []
 
-    wb.save(dep_name + ".xlsx")
+        # Retrieves course info for each department
+        for department in department_urls:
+            # Extracts the department name from the URL
+            dep_name = (department.rsplit('/', 2)[1]).upper()
+
+            # Gets raw list of courses and info for department
+            dep_link = 'http://catalog.calpoly.edu' + department
+            dep_soup = self.get_soup(dep_link)
+            courses = dep_soup.findAll("div", {"class": "courseblock"})
+
+            # Extract info for each class and populate spreadsheet
+            for course in courses:
+                course_name_and_units =\
+                    (course.find("p", {"class": "courseblocktitle"})).get_text()
+                course_name, course_units = course_name_and_units.splitlines()
+                course_units = course_units.split(' ', 1)[0]
+
+                course_terms_and_prereqs = (course.find(
+                    "div", {"class": "noindent courseextendedwrap"})).get_text()
+                try:
+                    course_terms, course_prereqs =\
+                        course_terms_and_prereqs.split("Prerequisite: ")
+                except IndexError:
+                    course_prereqs = "NA"
+                    course_terms = course_terms_and_prereqs\
+                        .split("Term Typically Offered: ", 1)[1]
+                except ValueError:
+                    # Yes this is the same behavior as an IndexError
+                    course_prereqs = "NA"
+                    course_terms = course_terms_and_prereqs\
+                        .split("Term Typically Offered: ", 1)[1]
+                else:
+                    course_terms = course_terms\
+                        .split("Term Typically Offered: ", 1)[1]
+
+                # Define data in a python dictionary
+                document = {
+                    "department": dep_name,
+                    "courseName": course_name,
+                    "units": course_units,
+                    "prerequisites": course_prereqs,
+                    "termsTypicallyOffered": course_terms
+                }
+
+                print("Storing course: {} with document {}".format(
+                    course_name, document))
+
+                # Create firebase Transaction type and add to list for
+                # batched write
+                firebase_transactions.append(Transaction(self.COLLECTION,
+                                                         course_name,
+                                                         document,
+                                                         self.DOCUMENT_TYPE))
+
+        # Call the firebase proxy interface to add course entries to
+        # firestore
+        self.firebase_proxy.batched_write(firebase_transactions)
